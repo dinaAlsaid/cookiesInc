387ec5d0338bd5a9b299f343812627721597fd18
dinaAlsaid <dinamalsaid@gmail.com>
2020-06-23 20:33:50 +0300

    added interactive content

---
+++ js/lab.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var name = prompt("what is your name");
+var currentH = new Date();
+var flag;
+document.write("<h3> Hello "+ name +" !!<h3>");
+
+
+if (currentH.getHours() >= 10 && currentH.getHours() <= 13){
+    if (13-currentH.getHours() == 1){
+        document.write("<h3> You have "+ (13-currentH.getHours())+ " hour to place an order <h3>");
+
+    }else{
+        document.write("<h3> You have "+ (13-currentH.getHours())+ " hours to place an order <h3>");
+    }
+     flag = true;
+} else {
+    document.write("<h3> Orders are closed <h3>");
+    flag = false;
+}
+
+if (flag){
+    confirm("you can place an order");
+} else {
+    confirm ("sorry we're closed");
+}
