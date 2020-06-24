9c491f2481d4a9cd90080b5ae73be51a392166e7
dinaAlsaid <dinamalsaid@gmail.com>
2020-06-24 12:17:38 +0300

    grouped functions and made sure inputed name doesnt have numbers

--- js/lab.js
+++ js/lab.js
@@ -1,35 +1,54 @@
 'use strict';
 
-var name = prompt("what is your name");
-var currentH = new Date();
+var day = new Date();
 var flag;
-document.write("<h3> Hello "+ name +" !!<h3>");
+function userName() {
+    var name = prompt("Name (don't use numbers)");
+    if (/\d/.test(name)) {  //not my code (from stack overflow)
+        ///checks if the string has any numbers/// if it has numbers (true ) if not false
+        return userName();  //will call the prompt again if there's numbers 
+    }else { 
+        return name; //there's no numbers will return the name
+         }
+}
+function storeOpen(currentH) {
+    if (currentH >= 10 && currentH <= 13) {
+        if (13 - currentH == 1) {
+            document.write("<h3> You have " + (13 - currentH) + " hour to place an order <h3>");
 
+        } else {
+            document.write("<h3> You have " + (13 - currentH) + " hours to place an order <h3>");
+        }
+        return true;
+    } else {
 
-if (currentH.getHours() >= 10 && currentH.getHours() <= 13){
-    if (13-currentH.getHours() == 1){
-        document.write("<h3> You have "+ (13-currentH.getHours())+ " hour to place an order <h3>");
+        document.write("<h3> Orders are closed <h3>");
 
-    }else{
-        document.write("<h3> You have "+ (13-currentH.getHours())+ " hours to place an order <h3>");
-    }
-     flag = true;
-} else {
+        if (currentH <= 10) {
+            document.write("<p> We will open in  " + (10 - currentH) + " hours <p>");
 
-    document.write("<h3> Orders are closed <h3>");
-    flag = false;
-    if (currentH.getHours() <= 10){
-        document.write("<p> We will open in  "+ (10-currentH.getHours())+ " hours <p>");
-    
-    } else if (currentH.getHours() >= 13){
-        document.write("<p> Come again tomorrow <p>");
-    
+        } else if (currentH >= 13) {
+            document.write("<p> Come again tomorrow <p>");
+
+        }
+        return false;
     }
 }
 
-if (flag){
-    confirm("you can place an order");
-} else {
-    confirm ("sorry we're closed");
+function welcomeNote(fl) {
+    if (fl) {
+        confirm("you can place an order");
+    } else {
+        confirm("sorry we're closed");
+    }
+
 }
+
+document.write("<h3> Hello " + userName() + " !!<h3>");
+
+flag = storeOpen(day.getHours());
+
+welcomeNote(flag);
+
+
 
